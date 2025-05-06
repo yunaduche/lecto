c24dbd23d721beb68e015b1a308bf05385ff30f8
yunaduche <yunaducherte@gmail.com>
2025-05-06 21:46:06 +0300

    nonsense

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@ exports.isAdmin = (req, res, next) => {
     if (req.user && req.user.role === 'admin') {
       next();
     } else {
-      res.status(403).json({ message: 'Accès refusé. Seuls les admins sont autorisés.' });
+      res.status(403).json({ message: 'Accès refusé. Seuls les admins sont autorisés. et riccardo aussi' });
     }
   };
   
